db94e9c554db084468a382a2038012b1c421ae4f
ludwig murimi <98152919+lu-dwig@users.noreply.github.com>
2025-02-03 09:18:13 +0000

    creation of the schema

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 'use strict';
 var expect = require('chai').expect;
+const { text } = require('body-parser');
 let mongodb = require('mongodb');
 let mongoose = require('mongoose');
 
@@ -7,6 +8,14 @@ module.exports = function (app) {
 
   let uri = 'mongodb+srv://Schleswig:' + process.env.PW + '@cluster0.u51m4.mongodb.net/Message-Board.message-board?retryWrites=true&w=majority&appName=Cluster0'
   mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let replySchema = new mongoose.Schema({
+  text: {type: String, required: true},
+  delete_password: {type: String, required: true},
+  createdon_: {type: Date, required: true},
+  reported: {type: Boolean, required: true}
+})
+  
   app.route('/api/threads/:board');
     
   app.route('/api/replies/:board');
