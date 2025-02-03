8095e1af55dc81d98ee5109674ee8528baa884be
ludwig murimi <98152919+lu-dwig@users.noreply.github.com>
2025-02-03 08:37:26 +0000

    database connection

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,12 @@
 'use strict';
+var expect = require('chai').expect;
+let mongodb = require('mongodb');
+let mongoose = require('mongoose');
 
 module.exports = function (app) {
-  
+
+  let uri = 'mongodb+srv://Schleswig:' + process.env.PW + '@cluster0.u51m4.mongodb.net/Message-Board.message-board?retryWrites=true&w=majority&appName=Cluster0'
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
   app.route('/api/threads/:board');
     
   app.route('/api/replies/:board');
